935f0fb9bad2d9fb60b5b48565b77fa905e8bc91
Хапачев Магамет <m.khapachev@soccard.ru>
2024-05-13 19:27:30 +0300

    Base Model changed

--- src/Libs/Models/BaseModel.php
+++ src/Libs/Models/BaseModel.php
@@ -10,22 +10,25 @@ namespace mhapach\SwaggerModelGenerator\Libs\Models;
 
 use Carbon\Carbon;
 use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionProperty;
+use ReflectionException;
+use stdClass;
 
 class BaseModel
 {
     /** @var bool - Строгое соответсвие полям модели */
     protected $strict = true;
-    /** @var string[]  - поля даты */
+
+    /** @var array - поля даты */
     protected $dates = [];
-    /** @var string[] - To include the attributes you need to specify these attributes to automatically append onto the response in your model */
-    protected $appends = [];
 
     /** @var array - mapping классов вида [поле из Soap => полный путь к классу] */
     protected $classMapping = [];
 
     public function toArray()
     {
-        return (array)$this;
+        return (array) $this;
     }
 
     public function toJson()
@@ -35,8 +38,10 @@ class BaseModel
 
     /**
      * BaseModel constructor.
-     * @param \stdClass|array $attributes
-     * @param array $classMapping
+     *
+     * @param  stdClass|array  $attributes
+     *
+     * @throws ReflectionException
      */
     public function __construct($attributes, array $classMapping = [])
     {
@@ -45,39 +50,54 @@ class BaseModel
          * те бывает так что у нас весь мэппинг определен в базовом классе
          * хотя лучше так не делать
          */
-        if (!is_array($this->classMapping))
-            $this->classMapping = [];
+        if (!empty($classMapping) && empty($this->classMapping)) {
+            $this->classMapping = $classMapping;
+        }
 
-        if (!empty($classMapping))
-            $this->classMapping = array_merge($this->classMapping, $classMapping);
-
-        if (!empty($attributes) && is_array($attributes))
+        if (!empty($attributes) && is_array($attributes)) {
             $this->autoFill($attributes);
-        else if (!empty($attributes) && $attributes instanceof \stdClass)
+        } elseif (!empty($attributes) && ($attributes instanceof stdClass)) {
             $this->autoFill(get_object_vars($attributes));
+        }
+
     }
 
     /**
      * Автозаполнение полей создающегося объека по аналогии с Ларой через конструктор
-     * @param array $attributes
+     *
+     *
+     * @throws ReflectionException
      */
-    protected function autoFill($attributes = array())
+    protected function autoFill(?array $attributes = []): void
     {
-        if (empty($attributes))
+//        dump($attributes);
+        if (empty($attributes)) {
             return;
+        }
+
+        $reflect = new ReflectionClass($this);
+        //        $this->props = $reflect->getProperties(ReflectionProperty::IS_PUBLIC);
 
         foreach ($attributes as $name => $value) {
             $name = trim($name);
-            $name = preg_replace('/\$/',"", $name);
-            if ($this->strict && !property_exists($this, $name))
+//            dump('---', $name, $value, '---');
+            $name = preg_replace('/\$/', '', $name);
+            if ($this->strict && ! property_exists($this, $name)) {
                 continue;
+            }
 
-            /** @var string $modelClassName  - Название модели наследнице класса BaseModel */
-            $modelClassName = isset($this->classMapping[$name]) ? $this->classMapping[$name] : ucfirst($name);
+            $prop = $reflect->getProperty($name);
+            $modelClassName = $this->propClassName($prop);
 
-            if ($modelClassName && class_exists($modelClassName) && !is_array($value))
+//            dump($name, $modelClassName, class_exists($modelClassName), self::isPropArray($prop));
+
+            if ($modelClassName && class_exists($modelClassName) && !self::isPropArray($prop)) {
+//                print "1 $name\n";
+//                dump(1, $value);
                 $this->$name = new $modelClassName($value, $this->classMapping);
-            else if ($modelClassName && class_exists($modelClassName) && is_array($value)) {
+            } elseif ($modelClassName && class_exists($modelClassName) && self::isPropArray($prop)) {
+//                print "1.1 $name\n";
+
                 $values = $value;
                 $valuesAsObjects = [];
                 foreach ($values as $item) {
@@ -85,39 +105,130 @@ class BaseModel
                 }
                 $this->$name = collect($valuesAsObjects);
             } else {
-                if (in_array($name, $this->dates) && !($value instanceof Carbon) && $value)
+//                print "2 $name\n";
+                if (in_array($name, $this->dates) && ! ($value instanceof Carbon) && $value) {
                     $value = new Carbon($value);
+                }
 
                 $this->$name = $value;
             }
         }
-
-        if (!empty($this->appends))
-            foreach ($this->appends as $field)
-                $this->$field = $this->$field;
-
-        return;
     }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function propClassName(ReflectionProperty $prop): ?string
+    {
+        $name = $prop->getName();
+        /** @var string $modelClassName - Название модели наследнице класса BaseModel */
+        $modelClassName = $this->classMapping[$name] ?? ucfirst($name);
+
+        /**
+         * пРОВЕРКА сделана для того, чтобы выяснить не является ли текущее имя property встроенным классом
+         * например public $number - будет преобразовано в сласс Number а реально допустим оно string
+         */
+        if (class_exists($modelClassName)) {
+            if (self::isPropScalar($prop)) {
+                return null;
+            }
+
+            return $modelClassName;
+        }
+
+        return null;
+    }
+
     /**
      * Чтобы срабатывали мутаторы Вида get{$snakeStyleName}Attribute
-     * @param $name
+     *
      * @return null
      */
     public function __get($name)
     {
         // name coming us with camel style thus we make em snake
-        $snakeStyleName = ucfirst(Str::camel($name));
+        $snakeStyleName = Str::camel($name);
         $methodName = "get{$snakeStyleName}Attribute";
 
         //Для relation
         if (method_exists($this, $name)) {
             return $this->$name();
-        }
-        //Для мутаторов
-        else if (method_exists($this, $methodName)) {
+        } //Для мутаторов
+        elseif (method_exists($this, $methodName)) {
             return $this->$methodName();
         }
+
         return null;
     }
 
+    public function isPropScalar(ReflectionProperty $prop): bool
+    {
+        if (self::isPropInteger($prop) || self::isPropFloat($prop) || self::isPropString($prop)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static function isPropInteger(ReflectionProperty $property): bool
+    {
+        if ($property->getType()?->getName() == 'int') {
+            return true;
+        }
+
+        $description = $property->getDocComment();
+
+        return str_contains(mb_strtolower($description), 'int');
+    }
+
+    private static function isPropString(ReflectionProperty $property): bool
+    {
+        if ($property->getType()?->getName() == 'string') {
+            return true;
+        }
+
+        $description = $property->getDocComment();
+
+        return str_contains(mb_strtolower($description), 'string');
+    }
+
+    private static function isPropFloat(ReflectionProperty $property): bool
+    {
+        if ($property->getType()?->getName() == 'float') {
+            return true;
+        }
+
+        $description = $property->getDocComment();
+
+        return str_contains(mb_strtolower($description), 'float');
+    }
+
+    private static function isPropDate(ReflectionProperty $prop): bool
+    {
+        if ($prop->getType()?->getName() == 'Date' || $prop->getType()->getName() == 'Carbon/Carbon') {
+            return true;
+        }
+
+        $description = $prop->getDocComment();
+
+        return str_contains(mb_strtolower($description), 'carbon');
+    }
+
+    public static function isPropArray(ReflectionProperty $prop): bool
+    {
+        if ($prop->getType()?->getName() == 'array') {
+            return true;
+        }
+
+        $description = $prop->getDocComment();
+
+        return str_contains(mb_strtolower($description), '[]') || str_contains(mb_strtolower($description), 'array');
+    }
+
+    public static function isPropMappedObject(ReflectionProperty $prop, ?array $classMapping = []): bool
+    {
+        $name = $prop->name;
+
+        return isset($classMapping[$name]);
+    }
 }
